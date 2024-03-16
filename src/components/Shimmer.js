ed5c0c35f9f3c805d1060da5506538c47065f644
Rashmita_piplani <rashmitapiplani@gmail.com>
2024-03-16 10:42:29 +0530

    till epsiode 6

---
+++ src/components/Shimmer.js
@@ -0,0 +1,12 @@
+const Shimmer = () => {
+  return (
+    <div className="shimmer-container">
+      <div className="shimmer-card">1</div>
+      <div className="shimmer-card">2</div>
+      <div className="shimmer-card">3</div>
+      <div className="shimmer-card">4</div>
+      <div className="shimmer-card">5</div>
+    </div>
+  );
+};
+export default Shimmer;
